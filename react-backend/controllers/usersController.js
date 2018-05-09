4231e28f257bf2f380630c86b3876c6ff0015c47
Elliott <e.tan@salesforce.com>
2018-05-08 19:22:16 -0700

    use npm, babel

---
+++ react-backend/controllers/usersController.js
@@ -0,0 +1,12 @@
+
+
+var test = function() {
+  return 'Hello from usersController';
+}
+
+var test2 = function() {
+  return 'test2 from usersController';
+}
+
+module.exports = {test: test, test2: test2};
+
